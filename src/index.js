aab8f1da2845e9a69d08c187d995224f14b28ee9
Fatanaminullah <fatan.aminullah.j@gmail.com>
2019-04-30 11:32:51 +0700

    deploy 2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req,res) => {
-  res.send("EXPRESS MONGOOSE BERJALAN DI HEROKU")
+  res.send(`<h1>EXPRESS MONGOOSE BERJALAN DI HEROKU DI PORT ${port}</h1>`)
 })
 
 app.post("/users", async (req, res) => {
