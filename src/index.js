a4b93395179c6a64c82f3f53cd090137611e5585
Fatanaminullah <fatan.aminullah.j@gmail.com>
2019-04-30 11:18:42 +0700

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req,res) => {
+  res.send("EXPRESS MONGOOSE BERJALAN DI HEROKU")
+})
+
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
